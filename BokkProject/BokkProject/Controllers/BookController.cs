a6a59d83606937c644ab672d46c52d2dc68396ed
rishi <rishipandit50@gmail.com>
2021-08-21 16:36:42 +0530

    Controller and Model Example

---
+++ BokkProject/BokkProject/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using BokkProject.Models;
+using BokkProject.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BokkProject.Controllers
+{
+    public class BookController : Controller
+    {
+
+        private readonly BookRepository _bookRepository = null;
+
+        public BookController()
+        
+        {
+            _bookRepository = new BookRepository();
+        }
+       
+        public List<BookModel> getAllBooks()
+        {
+           return _bookRepository.GetAllBooks();
+        }
+
+        public BookModel getSingleBook(int id)
+        {
+            return _bookRepository.GetSingleBook(id);
+        }
+
+        public List<BookModel> searchBook(string title, string author)
+        {
+            return _bookRepository.SerachBook(title, author);
+        }
+    }
+}
---
+++ BokkProject/BokkProject/Repository/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using BokkProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BokkProject.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return Datasource();
+        }
+        public BookModel GetSingleBook(int id)
+        {
+            return Datasource().Where(x => x.id == id).FirstOrDefault();
+        }
+        public List<BookModel> SerachBook(string title, string author)
+        {
+            return Datasource().Where(x => x.title.Contains(title) || x.author.Contains(author)).ToList();
+        }
+        private List<BookModel> Datasource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel() { id=1,title="C",author="Rishi"},
+                new BookModel() { id = 2, title = "Java", author = "Volvo" },
+                new BookModel() { id = 3, title = "C++", author = "Bejos" },
+                new BookModel() { id = 4, title = "Python", author = "Burk" },
+                new BookModel() { id = 5, title = "Node", author = "Rishi" }
+            };
+        }
+    }
+}
